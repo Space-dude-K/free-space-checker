9342bbc94dd3d15f75bf1ece60c26f6338c4eb46
Dude-sks <omicronyota@gmail.com>
2023-08-22 13:44:32 +0300

    Refactoring

--- ServerAvailabilityChecker.cs
+++ ServerAvailabilityChecker.cs
@@ -1,10 +1,6 @@
 ﻿using FreeSpaceChecker.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FreeSpaceChecker
 {
